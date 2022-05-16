705bdd9afa0b6383445bee3204927b547ac0ecb2
Donghee Lee <ehdgml9419@gmail.com>
2022-05-16 13:42:24 +0900

    Initial Commit

---
+++ SMMProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SMMProject
+//
+//  Created by 이동희 on 2022/05/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
