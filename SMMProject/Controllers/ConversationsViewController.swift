5dd9b4d7c391ee36843994074031d85c4928fa52
Donghee Lee <ehdgml9419@gmail.com>
2022-05-16 14:47:56 +0900

    set login, register UI

--- SMMProject/Controllers/ConversationsViewController.swift
+++ SMMProject/Controllers/ConversationsViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ConversationsViewController.swift
-//  SMMProject
+//  SMMProject: means Send My Messengsers
 //
 //  Created by 이동희 on 2022/05/16.
 //
@@ -11,7 +11,22 @@ class ConversationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        view.backgroundColor =  .red
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let logged_in = UserDefaults.standard.bool(forKey: "logged_in")
+        
+        if !logged_in {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false, completion: nil)
+        }
+    }
+    
+    
 }
 
---
+++ SMMProject/Resources/Extentions.swift
@@ -0,0 +1,36 @@
+//
+//  Extentions.swift
+//  SMMProject
+//
+//  Created by 이동희 on 2022/05/16.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    public var width: CGFloat {
+        return self.frame.size.width
+    }
+    
+    public var height: CGFloat {
+        return self.frame.size.height
+    }
+    
+    public var top: CGFloat {
+        return self.frame.origin.y
+    }
+    
+    public var bottom: CGFloat {
+        return self.frame.size.height + self.frame.origin.y
+    }
+    
+    public var left: CGFloat {
+        return self.frame.origin.x
+    }
+    
+    public var right: CGFloat {
+        return self.frame.size.width + self.frame.origin.x
+    }
+}
